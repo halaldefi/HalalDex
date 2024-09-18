4e351f6db1c1e2f8d8fb9b86696cee6addd29a50
Ahmed Hamza <hdamnit@gmail.com>
2024-09-18 17:32:51 +0530

    Remove unused debug layout, sentry page, wallet-check page, token-select page, remove-allowance page, token-input page, and alerts page

--- app/(app)/debug/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { DefaultPageContainer } from '@/lib/shared/components/containers/DefaultPageContainer'
-import { PropsWithChildren } from 'react'
-
-export default function DebugLayout({ children }: PropsWithChildren) {
-  return <DefaultPageContainer>{children}</DefaultPageContainer>
-}
