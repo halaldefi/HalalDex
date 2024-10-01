6bbe5a10a2dac366c6a1a1fd52b63151bae8d462
Ahmed Hamza <hdamnit@gmail.com>
2024-10-01 17:13:25 +0530

    Refactor ThirdwebConnectButton component to update ConnectButton styling and add custom theme

--- lib/modules/web3/ThirdwebConnectButton.tsx
+++ lib/modules/web3/ThirdwebConnectButton.tsx
@@ -1,16 +1,35 @@
-import { ConnectButton } from 'thirdweb/react'
+import { ConnectButton, darkTheme } from 'thirdweb/react'
 import { polygon, ethereum, arbitrum, bsc } from 'thirdweb/chains'
 import { client } from './ThirdwebConfig'
+import { Box, Button } from '@chakra-ui/react'
 
 export function ThirdwebConnectButton() {
   return (
-    <ConnectButton
-      client={client}
-      appMetadata={{
-        name: 'Halal IO',
-        url: 'https://halal.io',
-      }}
-      chains={[ethereum, polygon, bsc, arbitrum]}
-    />
+    <Button variant="tertiary" p="0">
+      <ConnectButton
+        client={client}
+        theme={darkTheme({
+          colors: {
+            primaryButtonBg: '#464d58',
+            primaryButtonText: '#fff',
+            connectedButtonBg: '#464d58',
+          },
+        })}
+        connectButton={{
+          label: 'Connect Wallet',
+          style: {
+            height: '40px',
+            background: 'transparent',
+            minWidth: '132px',
+            width: '132px',
+          },
+        }}
+        appMetadata={{
+          name: 'Halal IO',
+          url: 'https://halal.io',
+        }}
+        chains={[ethereum, polygon, bsc, arbitrum]}
+      />
+    </Button>
   )
 }
