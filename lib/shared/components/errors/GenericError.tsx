9298036781894a5295239aba6e66dbc081b4450c
Ahmed Hamza <hdamnit@gmail.com>
2024-10-02 20:22:47 +0530

    Refactor TransactionStateProvider to avoid unnecessary re-renders and side-effects

--- lib/shared/components/errors/GenericError.tsx
+++ lib/shared/components/errors/GenericError.tsx
@@ -2,7 +2,7 @@
 
 import { AlertProps, Text } from '@chakra-ui/react'
 import { ErrorAlert } from './ErrorAlert'
-import { isUserRejectedError, isViemHttpFetchError } from '../../utils/error-filters'
+import { isPausedError, isUserRejectedError, isViemHttpFetchError } from '../../utils/error-filters'
 import { ensureError } from '../../utils/errors'
 import { BalAlertLink } from '../alerts/BalAlertLink'
 
@@ -28,9 +28,20 @@ export function GenericError({ error: _error, customErrorName, ...rest }: Props)
       </ErrorAlert>
     )
   }
+  if (isPausedError(_error)) {
+    return (
+      <ErrorAlert title={customErrorName} {...rest}>
+        <Text variant="secondary" color="black">
+          The pool or one of the pool tokens is paused. Check{' '}
+          <BalAlertLink href="https://discord.balancer.fi/">our discord</BalAlertLink> for more
+          information.
+        </Text>
+      </ErrorAlert>
+    )
+  }
   const errorMessage = error?.shortMessage || error.message
 
-  if (errorMessage === 'RPC Request failed.') {
+  if (errorMessage === 'RPC Request failed.' || errorMessage === 'An unknown RPC error occurred.') {
     return (
       <ErrorAlert title={errorMessage} {...rest}>
         <Text variant="secondary" color="black">
